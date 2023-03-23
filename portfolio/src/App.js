b734ed803357d8f48df909261b86fd681b14e24f
Rahul Ohol <rahulohol01@gmail.com>
2023-03-23 13:46:13 +0530

    m

---
+++ portfolio/src/App.js
@@ -0,0 +1,29 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import "./App.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Skills } from "./components/Exp/Exp";
+import { Footer } from "./components/Footer/Footer";
+import { Hero } from "./components/Hero/Hero";
+import { Particle } from "./components/Hero/Particle";
+import { Nav } from "./components/Nav/Nav";
+import { Project } from "./components/Project/Project";
+import { GithubCalender } from "./components/Stats/GithubCalender";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Particle />
+      <Nav /> 
+       <Hero />
+      <About />
+      <Skills />
+      <GithubCalender />
+      <Project />
+      <Contact />
+      <Footer />
+    </ChakraProvider>
+  );
+}
+
+export default App;
